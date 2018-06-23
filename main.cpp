5ebfb9bf58b880b884ff642496989e06ea2f5309
Ly Hai Nam <hainamlycb080597@gmail.com>
2018-06-23 13:58:49 +0700

    Main program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+	TITLE: Bai tap lon KY THUAT LAP TRINH
+	Semester: 20161
+	ID Course: MI3310
+	Author: Nhom 9 (Pham Vu Tien, Ly Hai Nam, Nguyen Lam Tung)
+*/
+
+#include "menu.h"
+
+int main()
+{
+	menu();
+	getch();
+	return 0;
+}
+
